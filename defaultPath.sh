3ca1c070cb32fbd3274e9d1c38095ac749325263
flotisable <s09930698@gmail.com>
2018-12-02 22:10:55 +0800

    fix: default path on windows

--- defaultPath.sh
+++ defaultPath.sh
@@ -5,7 +5,7 @@ os=$1
 case ${os} in
 
   linux   ) echo "${HOME}/.config/Code/User";;
-  windows ) echo "%APPDATA%\\Code\\User";;
+  windows ) echo "${APPDATA}\\Code\\User";;
   macos   ) echo "${HOME}/Library/Application Support/Code/User";;
 
 esac
