9e661be70383536627c317d9a9240fad5b5604ba
flotisable <s09930698@gmail.com>
2018-12-25 20:13:02 +0800

    fix: install error on macos

--- install.sh
+++ install.sh
@@ -10,22 +10,23 @@ fi
 vscodeSettingPath=${vscodeSettingsDir}/${vscodeSettingName}
 vscodeKeybindingPath=${vscodeSettingsDir}/${vscodeKeybindingName}
 vscodeLocalePath=${vscodeSettingsDir}/${vscodeLocaleName}
+
 # end setup variables 
 
 # install
 if [ ${installSetting} -eq 1 ]; then
   echo "install setting"
-  cp ./${vscodeSettingSource} ${vscodeSettingPath}
+  cp "./${vscodeSettingSource}" "${vscodeSettingPath}"
 fi
 
 if [ ${installKeybinding} -eq 1 ]; then
   echo "install keybinding"
-  cp ./${vscodeKeybindingSource} ${vscodeKeybindingPath}
+  cp "./${vscodeKeybindingSource}" "${vscodeKeybindingPath}"
 fi
 
 if [ ${installLocale} -eq 1 ]; then
   echo "install locale"
-  cp ./${vscodeLocaleSource} ${vscodeLocalePath}
+  cp "./${vscodeLocaleSource}" "${vscodeLocalePath}"
 fi
 
 while read extension; do
