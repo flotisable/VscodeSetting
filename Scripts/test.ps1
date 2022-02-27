f6dd5821f8c73db83df265b0d8fd7229483e58b6
flotisable <s09930698@gmail.com>
2022-02-27 15:02:12 +0800

    add test for makefile sync related targets

--- Scripts/test.ps1
+++ Scripts/test.ps1
@@ -2,6 +2,9 @@ $oses = "Linux",
         "Windows",
         "Macos"
 
+Set-Variable -Option Constant passColor 'Green'
+Set-Variable -Option Constant failColor 'Red'
+
 Function osNameToOsEnv( $os )
 {
   Switch( ${os} )
@@ -12,16 +15,134 @@ Function osNameToOsEnv( $os )
   }
 }
 
-Function testMakefileTarget( $target )
+Function testInput( $target )
 {
-  Write-Host "[Test makefile target '$target']"
-  make --no-print-directory $target
+  Switch( $target )
+  {
+    'sync-main-from-local' { Write-Output ':qa' }
+  }
 }
 
-$testDir     = "Test"
-$sourceDir   = "${testDir}/Source"
-$targetDir   = "${testDir}/Target"
-$targetFile  = "testTarget.txt"
+Function testMakefileTarget( $target )
+{
+  Switch( $target )
+  {
+    'sync-main-to-local'    { prepareTestSyncRemoteToLocal      > $null 2>&1 }
+    'sync-main-from-local'  { prepareTestSyncMainFromLocal      > $null 2>&1 }
+    'sync-to-local'         { prepareTestSyncMainToLocalMachine > $null 2>&1 }
+  }
+
+  Write-Host "[Test makefile target '$target']"
+  testInput $target | make --no-print-directory $target > $null 2>&1
+
+  If( $LastExitCode -eq 0 )
+  {
+    Write-Host -ForegroundColor $passColor 'Test pass'
+  }
+  Else
+  {
+    Write-Host -ForegroundColor $failColor 'Test fail'
+  }
+
+  Switch( $target )
+  {
+    'sync-main-to-local'    { cleanupTestSyncRemoteToLocal      }
+    'sync-main-from-local'  { cleanupTestSyncMainFromLocal      }
+    'sync-to-local'         { cleanupTestSyncMainToLocalMachine }
+  }
+}
+
+Function prepareTestSyncRemoteToLocal()
+{
+  git clone . ${osTargetDir}/remote
+  git clone . ${osTargetDir}/local
+  Set-Location ${osTargetDir}/local
+  git config --local commit.gpgsign             false
+  git config --local pull.rebase                false
+  git config --local branch.main.mergeOptions   '--strategy=ort --strategy-option=ours'
+  git config --local branch.local.mergeOptions  '--strategy=ort --strategy-option=ours'
+  git branch -m main
+  git branch local
+  git checkout main
+  git remote add remote ../remote
+  git fetch remote
+  git branch --set-upstream-to=remote/main
+  $(Get-Content makefile) -replace '(remoteBranch\s+:=\s+)\$.+$', '${1}remoteMain' | Set-Content makefile
+  git commit -am 'change remote branch'
+  Set-Location ../remote
+  git branch -m main
+  git config --local commit.gpgsign false
+  $(Get-Content makefile) -replace '(remoteBranch\s+:=\s+)\$.+$', '${1}remoteRemote' | Set-Content makefile
+  git commit -am 'change remote branch'
+  Set-Location ../local
+  git checkout local
+  $(Get-Content makefile) -replace '(mainBranch\s+:=\s+)master',  '${1}main' | Set-Content makefile
+  $(Get-Content makefile) -replace '(remoteBranch\s+:=\s+)\$.+$', '${1}main' | Set-Content makefile
+  git commit -am 'change remote branch'
+}
+
+Function cleanupTestSyncRemoteToLocal()
+{
+  Set-Location ${rootDir}
+}
+
+Function prepareTestSyncMainFromLocal()
+{
+  git clone . ${osTargetDir}/main
+  Set-Location ${osTargetDir}/main
+  git branch -m main
+  git config --local commit.gpgsign           false
+  git config --local branch.main.mergeOptions '--strategy=ort --strategy-option=ours'
+  $(Get-Content makefile) -replace '(mainBranch\s+:=\s+)master', '${1}main' | Set-Content makefile
+  git commit -am 'change main branch'
+  git checkout -b local
+  $(Get-Content makefile) -replace '(remoteBranch\s+:=\s+)\$.+$', '${1}remoteLocal' | Set-Content makefile
+  git commit -am 'change remote branch'
+  git checkout main
+  $(Get-Content makefile) -replace '(remoteBranch\s+:=\s+)\$.+$', '${1}remoteMain' | Set-Content makefile
+  git commit -am 'change remote branch'
+  git checkout local
+  $(Get-Content makefile) -replace '(remoteBranch\s+:=\s+)remoteLocal', '${1}remoteLocalForStash' | Set-Content makefile
+}
+
+Function cleanupTestSyncMainFromLocal()
+{
+  Set-Location ${rootDir}
+}
+
+Function prepareTestSyncMainToLocalMachine()
+{
+  git clone . ${osTargetDir}/toLocalMachine
+  Set-Location ${osTargetDir}/toLocalMachine
+  git branch -m main
+  New-Item -ItemType Directory -Force ${sourceDir}
+  New-Item ${sourceDir}/testSource.txt
+  New-Item -ItemType Directory -Force ${osTargetDir}
+  git config --local commit.gpgsign             false
+  git config --local branch.local.mergeOptions  '--strategy=ort --strategy-option=ours'
+  $(Get-Content makefile) -replace '(mainBranch\s+:=\s+)master', '${1}main' | Set-Content makefile
+  git commit -am 'change main branch'
+  git branch local
+  $(Get-Content makefile) -replace '(remoteBranch\s+:=\s+)\$.+$', '${1}remoteMain' | Set-Content makefile
+  git commit -am 'change remote branch'
+  git checkout local
+  $(Get-Content makefile) -replace '(remoteBranch\s+:=\s+)\$.+$', '${1}remoteLocal' | Set-Content makefile
+  git commit -am 'change remote branch'
+  git checkout main
+  $(Get-Content makefile) -replace '(remoteBranch\s+:=\s+)remoteMain', '${1}main' | Set-Content makefile
+}
+
+Function cleanupTestSyncMainToLocalMachine()
+{
+  Set-Location ${rootDir}
+}
+
+$env:GIT_EDITOR = 'powershell -NoProfile -Command Write-Host'
+$testDir        = "Test"
+$sourceDir      = "${testDir}/Source"
+$targetDir      = "${testDir}/Target"
+$targetFile     = "testTarget.txt"
+$rootDir        = Get-Location
 
 New-Item -ItemType Directory -Force ${sourceDir} > $null
 
@@ -36,5 +157,8 @@ ForEach( $os in ${oses} )
   testMakefileTarget copy
   testMakefileTarget uninstall
   testMakefileTarget install
+  testMakefileTarget sync-main-to-local
+  testMakefileTarget sync-main-from-local
+  testMakefileTarget sync-to-local
 }
 Remove-Item -Recurse -Force ${testDir}
--- Scripts/test.sh
+++ Scripts/test.sh
@@ -18,18 +18,162 @@ osNameToOsEnv()
   esac
 }
 
+setPassColor()
+{
+  if [ -t 1 ]; then
+    tput setaf 2
+  fi
+}
+
+setFailColor()
+{
+  if [ -t 1 ]; then
+    tput setaf 1
+  fi
+}
+
+resetColor()
+{
+  if [ -t 1 ]; then
+    tput op
+  fi
+}
+
+testInput()
+{
+  local target=$1
+
+  case $target in
+
+    sync-main-from-local) echo ':qa';;
+
+  esac
+}
+
 testMakefileTarget()
 {
   local target=$1
 
+  case $target in
+
+    sync-main-to-local)   prepareTestSyncRemoteToLocal      > /dev/null 2>&1;;
+    sync-main-from-local) prepareTestSyncMainFromLocal      > /dev/null 2>&1;;
+    sync-to-local)        prepareTestSyncMainToLocalMachine > /dev/null 2>&1;;
+
+  esac
+
   echo "[Test makefile target '$target']"
-  make --no-print-directory $target
+  testInput $target | make --no-print-directory $target > /dev/null 2>&1
+
+  if [ $? -eq 0 ]; then
+    setPassColor
+    echo 'Test pass'
+  else
+    setFailColor
+    echo 'Test fail'
+  fi
+  resetColor
+
+  case $target in
+
+    sync-main-to-local)   cleanupTestSyncRemoteToLocal;;
+    sync-main-from-local) cleanupTestSyncMainFromLocal;;
+    sync-to-local)        cleanupTestSyncMainToLocalMachine;;
+
+  esac
 }
+
+prepareTestSyncRemoteToLocal()
+{
+  git clone . ${osTargetDir}/remote
+  git clone . ${osTargetDir}/local
+  cd ${osTargetDir}/local
+  git config --local commit.gpgsign             false
+  git config --local pull.rebase                false
+  git config --local branch.main.mergeOptions   '--strategy=ort --strategy-option=ours'
+  git config --local branch.local.mergeOptions  '--strategy=ort --strategy-option=ours'
+  git branch -m main
+  git branch local
+  git checkout main
+  git remote add remote ../remote
+  git fetch remote
+  git branch --set-upstream-to=remote/main
+  sed -i '/remoteBranch\s\+:=/ s/\$.\+$/remoteMain/' makefile
+  git commit -am 'change remote branch'
+  cd ../remote
+  git branch -m main
+  git config --local commit.gpgsign false
+  sed -i '/remoteBranch\s\+:=/ s/\$.\+$/remoteRemote/' makefile
+  git commit -am 'change remote branch'
+  cd ../local
+  git checkout local
+  sed -i '/mainBranch/ s/master/main/;/remoteBranch\s\+:=/ s/\$.\+$/main/' makefile
+  git commit -am 'change remote branch'
+}
+
+cleanupTestSyncRemoteToLocal()
+{
+  cd ${rootDir}
+}
+
+prepareTestSyncMainFromLocal()
+{
+  git clone . ${osTargetDir}/main
+  cd ${osTargetDir}/main
+  git branch -m main
+  git config --local commit.gpgsign           false
+  git config --local branch.main.mergeOptions '--strategy=ort --strategy-option=ours'
+  sed -i '/mainBranch\s\+:=/ s/master/main/' makefile
+  git commit -am 'change main branch'
+  git checkout -b local
+  sed -i '/remoteBranch\s\+:=/ s/\$.\+$/remoteLocal/' makefile
+  git commit -am 'change remote branch'
+  git checkout main
+  sed -i '/remoteBranch\s\+:=/ s/\$.\+$/remoteMain/' makefile
+  git commit -am 'change remote branch'
+  git checkout local
+  sed -i '/remoteBranch\s\+:=/ s/remoteLocal/remoteLocalForStash/' makefile
+}
+
+cleanupTestSyncMainFromLocal()
+{
+  cd ${rootDir}
+}
+
+prepareTestSyncMainToLocalMachine()
+{
+  git clone . ${osTargetDir}/toLocalMachine
+  cd ${osTargetDir}/toLocalMachine
+  git branch -m main
+  mkdir -p ${sourceDir}
+  touch ${sourceDir}/testSource.txt
+  mkdir -p ${osTargetDir}
+  git config --local commit.gpgsign             false
+  git config --local branch.local.mergeOptions  '--strategy=ort --strategy-option=ours'
+  sed -i '/mainBranch\s\+:=/ s/master/main/' makefile
+  git commit -am 'change main branch'
+  git branch local
+  sed -i '/remoteBranch\s\+:=/ s/\$.\+$/remoteMain/' makefile
+  git commit -am 'change remote branch'
+  git checkout local
+  sed -i '/remoteBranch\s\+:=/ s/\$.\+$/remoteLocal/' makefile
+  git commit -am 'change remote branch'
+  git checkout main
+  sed -i '/remoteBranch\s\+:=/ s/remoteMain/main/' makefile
+}
+
+cleanupTestSyncMainToLocalMachine()
+{
+  cd ${rootDir}
+}
+
+export GIT_EDITOR='cat'
 
 testDir="Test"
 sourceDir="${testDir}/Source"
 targetDir="${testDir}/Target"
 targetFile="testTarget.txt"
+rootDir=$(pwd)
 
 mkdir -p ${sourceDir}
 
@@ -44,6 +188,9 @@ for os in ${oses}; do
   testMakefileTarget copy
   testMakefileTarget uninstall
   testMakefileTarget install
+  testMakefileTarget sync-main-to-local
+  testMakefileTarget sync-main-from-local
+  testMakefileTarget sync-to-local
 
 done
 
